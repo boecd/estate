888c195aa4978ba57ba05c5747309a7c14a68dd3
bocde <bo.werth@gmail.com>
2013-10-06 13:26:10 +0200

    adding other files

---
+++ R/estate-package.R
@@ -0,0 +1,23 @@
+#' Real estate.
+#'
+#' \tabular{ll}{
+#' Package: \tab estate\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2013-09-26\cr
+#' Depends: \tab R (>= 2.10.0)\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab http://spark.rstudio.com\cr
+#' }
+#'
+#' Provides a function to scrape information from pap.fr.
+#'
+#' @aliases estate-package estate
+#' @name estate-package
+#' @docType package
+#' @title The estate Package
+#' @author Bo Werth \email{bo.werth@@gmail.com}
+#' @keywords package
+NULL
